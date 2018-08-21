5e8da7484e678e31ba2035b2c25adcffe3965d6d
zhaochunyan123 <460828791@qq.com>
2018-08-21 16:11:04 +0800

    no message

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -4,10 +4,10 @@ class TestLogin:
         assert 1
 
     def test_login2(self):
-        assert 0
+        assert 1
 
     def test_login3(self):
         assert 1
 
     def test_login4(self):
-        assert 0
+        assert 1
